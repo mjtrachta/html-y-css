c4d3f92a3c356c7fa6520468709af215da9b942d
mjtrachta <61702195+mjtrachta@users.noreply.github.com>
2023-02-03 13:21:06 -0300

    script cambio de color

---
+++ script.js
@@ -0,0 +1,7 @@
+let x = 1
+console.log(x)
+
+document.getElementById("enviar").addEventListener("click", function() {
+    this.style.backgroundColor = this.style.backgroundColor === "blue" ? "blue" : "blue";
+  });
+
